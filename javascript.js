115d699e34b3c25cf6f41d7a6fb098897c383908
Eswar Yaganti <nagamalli9999@hotmail.com>
2016-10-29 16:27:13 -0400

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -1,47 +1,82 @@
 angular.module('portalApp')
-.controller('highStreet40Ctrl', ['$scope', function ($scope) {
-	
-	// mock data
-	$scope.items = [
-		{
-			title:'Item 1',
-			tags: ['tag A', 'tag B', 'tag C'],
-			details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-		},
-		{
-			title:'Item 2',
-			tags: ['tag D', 'tag E', 'tag F'],
-			details: 'Mauris cursus, sapien et malesuada ultrices, purus sapien iaculis tellus, quis semper magna est at leo.'
-		},
-		{
-			title:'Item 3',
-			tags: ['tag A', 'tag H'],
-			details: 'Donec id quam eu odio feugiat sagittis. Duis a tempus neque. Praesent elementum quis ante quis commodo. Sed tincidunt aliquet dolor sit amet laoreet. '
-		},
-		{
-			title:'Item 4',
-			tags: ['tag I'],
-			details: 'Proin sem quam, rutrum id ante id, scelerisque tempor quam. Curabitur pharetra turpis at sem placerat, non vehicula ligula tincidunt.'
-		},
-		{
-			title:'Item 5',
-			tags: ['tag C', 'tag K', 'tag B'],
-			details: 'Mauris nec ultricies metus. Cras et dictum justo. Nam a ullamcorper dolor. Cras fringilla metus vel facilisis vehicula.'
-		},
-		{
-			title:'Item 6',
-			tags: ['tag A', 'tag B', 'tag C'],
-			details: 'Curabitur scelerisque lorem risus, in luctus orci hendrerit non. Praesent quis tellus dapibus dolor consectetur volutpat.'
-		}
-	];
-	
-	// Show main view in the first column as soon as controller loads
-	$scope.portalHelpers.showView('highStreet40Main.html', 1);
-	
-	// This function gets called when user clicks an item in the list
-	$scope.showDetails = function(item){
-		// Make the item that user clicked available to the template
-		$scope.detailsItem = item;		
-		$scope.portalHelpers.showView('highStreet40Details.html', 2);
-	}
-}]);
+    .controller('highStreet40Ctrl', function($scope) {
+
+        $scope.portalHelpers.showView("highStreet40Main.html", 1);
+
+        $scope.displayRegistration = {
+            value: false
+        };
+        $scope.displayMatches = {
+            value: false
+        };
+        $scope.displayFinal = {
+            value: true
+        };
+        $scope.profiles = [{
+            "name": "Rose",
+            "age": 23,
+            "image": "https://randomuser.me/api/portraits/lego/6.jpg",
+            "tag": "Tall",
+        }, {
+            "name": "Chang",
+            "age": 22,
+            "image": "https://randomuser.me/api/portraits/lego/0.jpg",
+            "tag": "Funny"
+        }, {
+            "name": "Shinqin",
+            "age": 23,
+            "image": "https://randomuser.me/api/portraits/lego/1.jpg",
+            "tag": "Smart"
+        }, {
+            "name": "Williams",
+            "age": 26,
+            "image": "https://randomuser.me/api/portraits/lego/2.jpg",
+            "tag": "Kind"
+        }, {
+            "name": "Emma",
+            "age": 25,
+            "image": "https://randomuser.me/api/portraits/lego/3.jpg",
+            "tag": "Beautiful"
+        }, {
+            "name": "Jennifer",
+            "age": 28,
+            "image": "https://randomuser.me/api/portraits/lego/4.jpg",
+            "tag": "Cool"
+        }];
+        $scope.remove = function(array) {
+            array.splice(array, 1);
+            if (array.length === 0) {
+                $scope.displayMatches.value = false;
+                $scope.displayFinal.value = true;
+            }
+        };
+
+        $scope.profs = [{
+            userid: 1,
+            username: "Stephany",
+            userurl: "https://randomuser.me/api/portraits/lego/5.jpg",
+            skipped: "No",
+            age: "22",
+            bio: "Lorem ipsum",
+            connect: "https:///"
+        }];
+        $scope.prevProfiles = [{
+            userid: 1,
+            username: "Kira",
+            userurl: "https://randomuser.me/api/portraits/lego/5.jpg",
+            skipped: "No",
+            wathot: "7.8"
+        }, {
+            userid: 2,
+            username: "John",
+            userurl: "https://randomuser.me/api/portraits/lego/8.jpg",
+            skipped: "No",
+            wathot: "5.9"
+        }, {
+            userid: 3,
+            username: "Katey",
+            userurl: "https://randomuser.me/api/portraits/lego/2.jpg",
+            skipped: "No",
+            wathot: "7.5"
+        }];
+    });
